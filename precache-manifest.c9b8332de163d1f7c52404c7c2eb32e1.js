8f8446b319e47099af875aae68fc6a38b881dd03
aliobba <ali.obba@esprit.tn>
2021-08-11 12:44:14 +0100

    Updates

---
+++ precache-manifest.c9b8332de163d1f7c52404c7c2eb32e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4318054cb733d57888989621c9f8197",
+    "url": "/pwa-react/index.html"
+  },
+  {
+    "revision": "78356dd10275d3fc17ef",
+    "url": "/pwa-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b31c2d3ea908c8286a5a",
+    "url": "/pwa-react/static/css/main.63598071.chunk.css"
+  },
+  {
+    "revision": "78356dd10275d3fc17ef",
+    "url": "/pwa-react/static/js/2.68ebac8a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pwa-react/static/js/2.68ebac8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b31c2d3ea908c8286a5a",
+    "url": "/pwa-react/static/js/main.75e86791.chunk.js"
+  },
+  {
+    "revision": "ec789973cab11c1f92da",
+    "url": "/pwa-react/static/js/runtime-main.39551e8a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/pwa-react/static/media/logo.ee7cd8ed.svg"
+  }
+]);
